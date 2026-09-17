f491789d69f7d16b95638df7981b4fa40a788721
Eshaan Issar <eshaanissar@outlook.com>
2026-09-17 00:12:14 -0400

    chore: rebuild userscript for v0.1.0

--- dist/bytm-plugin-playlist-sidebar.user.js
+++ dist/bytm-plugin-playlist-sidebar.user.js
@@ -6,15 +6,15 @@
 // @description  Sorts and filters the YouTube Music sidebar playlist list, with server-backed Recently Played and Recent Activity orders.
 // @license      MIT
 // @copyright    Copyright 2026 eissar
-// @icon         https://raw.githubusercontent.com/eissar/bytm-plugin-playlist-sidebar/357d167/assets/plugin_icon_128x128.png#sha256=4GgH3wuDgVjYVPf1s6NURcDU0QvjnLCigrlKowsF6x8=
+// @icon         https://raw.githubusercontent.com/eissar/bytm-plugin-playlist-sidebar/bbc3851/assets/plugin_icon_128x128.png#sha256=4GgH3wuDgVjYVPf1s6NURcDU0QvjnLCigrlKowsF6x8=
 // @homepage     https://github.com/eissar/bytm-plugin-playlist-sidebar
 // @homepageURL  https://github.com/eissar/bytm-plugin-playlist-sidebar
 // @source       https://github.com/eissar/bytm-plugin-playlist-sidebar.git
 // @supportURL   https://github.com/eissar/bytm-plugin-playlist-sidebar/issues
 // @match        https://youtube.com/*
 // @match        https://music.youtube.com/*
-// @resource     icon_1000  https://raw.githubusercontent.com/eissar/bytm-plugin-playlist-sidebar/357d167/assets/plugin_icon_1000x1000.png#sha256=IrFR29ZTCXuH5WsSVcmPn5FA+GvBopOyGR9lFSi4s5c=
-// @resource     icon_128   https://raw.githubusercontent.com/eissar/bytm-plugin-playlist-sidebar/357d167/assets/plugin_icon_128x128.png#sha256=4GgH3wuDgVjYVPf1s6NURcDU0QvjnLCigrlKowsF6x8=
+// @resource     icon_1000  https://raw.githubusercontent.com/eissar/bytm-plugin-playlist-sidebar/bbc3851/assets/plugin_icon_1000x1000.png#sha256=IrFR29ZTCXuH5WsSVcmPn5FA+GvBopOyGR9lFSi4s5c=
+// @resource     icon_128   https://raw.githubusercontent.com/eissar/bytm-plugin-playlist-sidebar/bbc3851/assets/plugin_icon_128x128.png#sha256=4GgH3wuDgVjYVPf1s6NURcDU0QvjnLCigrlKowsF6x8=
 // @connect      i.ytimg.com
 // @connect      youtube.com
 // @connect      github.com
@@ -106,7 +106,7 @@
   }
   const rawConsts = {
     buildMode: "production",
-    buildNumber: "357d167"
+    buildNumber: "bbc3851"
   };
   const getConst = (constKey, defaultVal) => {
     const val = rawConsts[constKey];
